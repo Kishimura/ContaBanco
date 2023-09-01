5890b723a149d2d13d68d58e7c11e0beba36c121
kishimura <tiagowinchesterx@hotmail.com>
2023-08-31 23:33:23 -0300

    commit 2

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner texto = new Scanner(System.in);
+
+
+        System.out.println("Por favor, digite o número da sua conta");
+        int numeroConta = texto.nextInt();
+        
+        System.out.println("Por favor, digite o número da sua agência");
+        int agencia = texto.nextInt();
+
+        System.out.println("Por favor, digite o seu nome");
+        String nomeCliente = texto.nextLine();
+
+        System.out.println("Por favor, digite o seu saldo");
+        double saldoConta =  texto.nextDouble();
+
+        String mensagem = "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +
+        ", conta " + numeroConta + " e seu saldo " + saldoConta + " já está disponível para saque.";
+
+        System.out.println(mensagem);
+        
     }
 }
