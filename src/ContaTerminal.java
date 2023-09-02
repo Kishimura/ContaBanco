2dda9e05e197510e5da18cb925ed220423efb516
kishimura <tiagowinchesterx@hotmail.com>
2023-09-01 21:48:04 -0300

    final-commit

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -8,15 +8,18 @@ public class ContaTerminal {
 
         System.out.println("Por favor, digite o número da sua conta");
         int numeroConta = texto.nextInt();
+        texto.nextLine();
         
-        System.out.println("Por favor, digite o número da sua agência");
-        int agencia = texto.nextInt();
+        System.out.println("Por favor, agora digite o número da sua agência");
+        String agencia = texto.nextLine();
 
-        System.out.println("Por favor, digite o seu nome");
+        System.out.println("Por favor, digite o seu nome:");
         String nomeCliente = texto.nextLine();
 
+        
         System.out.println("Por favor, digite o seu saldo");
         double saldoConta =  texto.nextDouble();
+
 
         String mensagem = "Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +
         ", conta " + numeroConta + " e seu saldo " + saldoConta + " já está disponível para saque.";
